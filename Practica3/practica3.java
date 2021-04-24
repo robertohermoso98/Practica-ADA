e1452bd37dc8af8f8d4161ccb2467b70f72c717f
Roberto <robertohermoso98@gmail.com>
2021-04-24 18:32:19 +0200

    Ya esta lista la entrada de datos del problema

--- Practica3/practica3.java
+++ Practica3/practica3.java
@@ -3,9 +3,29 @@ import java.util.*;
 
 public class practica3 {
     public static void main(String [] args){
-        System.out.println("Hola mundo");
+        Scanner sc = new Scanner(System.in);
+        String [] linea1=sc.nextLine().split(" ");
+        Integer [][] lin=new Integer[linea1.length][linea1.length];
+        for(int i = 0; i<linea1.length; i++){
+            lin[0][i]=Integer.parseInt(linea1[i]);
+        }
+        for(int e =0; e<linea1.length-1; e++){
+           String [] sigLine=sc.nextLine().split(" ");
+            for(int i =0; i<sigLine.length; i++){
+               lin[e][i]=Integer.parseInt(sigLine[i]);
+            }
+        }
+         String ss =conjuntoMinimo(lin);
+    }
+    public static String conjuntoMinimo(Integer[][] matrix){
+        String solucion="";
 
-        ente();
+
+
+        
+
+
+        return solucion;
     }
 
     
